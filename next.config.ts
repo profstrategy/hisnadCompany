715e6ac54f10f143bd8b523abbc12e0b3074ec27
profstrategy <ismailabdulrahman555@gmail.com>
2025-07-15 18:11:55 +0100

    hot-fix: update build script and next config for deployment

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
   webpack: (config) => {
     config.externals = [...config.externals, '@prisma/client']
     return config
+  },
+
+   experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
   }
 
 };
