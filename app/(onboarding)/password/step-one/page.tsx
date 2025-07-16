aa111b25e9bfeea285c31b017f13ae28e8510578
profstrategy <ismailabdulrahman555@gmail.com>
2025-07-16 17:30:26 +0100

    revamp password flow

--- app/(onboarding)/password/step-one/page.tsx
+++
@@ -1,8 +0,0 @@
-import ResetPasswordStepOne from '@/app/(public-pages)/_components/password/reset-password-step-one'
-import React from 'react'
-
-export default function StepOne() {
-  return (
-    <div><ResetPasswordStepOne /></div>
-  )
-}
